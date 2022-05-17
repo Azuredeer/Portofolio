aee73a238603da92ef3b8ba1294dda4259285ccb
yoanrifqi <yoanrifqicandra@gmail.com>
2022-05-17 13:39:36 +0700

    Commit1

---
+++ resources/views/Portofolio.blade.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Portofolio</title>
+    <link rel="icon" type="image/x-icon" href="{{ URL('asset/images/assets/img/favicon.ico')}}">
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="{{ URL :: asset('asset/styles/styles.css')}}" rel="stylesheet" />
+</head>
+
+<body id="page-top">
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">
+            <span class="d-block d-lg-none">Yoan Rifqi Candra</span>
+            <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="{{ URL('asset/images/assets/img/profile.jpg')}}" alt="..." /></span>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">Experience</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">Education</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#interests">Interests</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#awards">Awards</a></li>
+            </ul>
+        </div>
+    </nav>
+    <!-- Page Content-->
+    <div class="container-fluid p-0">
+        <!-- About-->
+        <section class="resume-section" id="about">
+            <div class="resume-section-content">
+                <h1 class="mb-0">
+                    Yoan Rifqi
+                    <span class="text-primary">Candra</span>
+                </h1>
+                <div class="subheading mb-5">
+                    Jln. Petuah IV · Jeulingke, 23114 · 0823-6628-6069 ·
+                    <a href="mailto:yoanrifqicandra@email.com">yoanrifqicandra@email.com</a>
+                </div>
+                <p class="lead mb-5">.</p>
+                <div class="social-icons">
+                    <a class="social-icon" href="https://www.linkedin.com/in/yoan-rifqi-candra-84787023a/"><i class="fab fa-linkedin-in"></i></a>
+                    <a class="social-icon" href="https://github.com/Azuredeer"><i class="fab fa-github"></i></a>
+                    <a class="social-icon" href="https://www.instagram.com/yoanrifqi/"><i class="fab fa-instagram"></i></a>
+                </div>
+            </div>
+        </section>
+        <hr class="m-0" />
+        <!-- Experience-->
+        <section class="resume-section" id="experience">
+            <div class="resume-section-content">
+                <h2 class="mb-5">Experience</h2>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">Sekolah Menengah Atas</h3>
+                        <p>Saya siswa jurusan ilmu pengetahuan alam di SMA Negeri 1 Lhokseumawe. Saya telah bersekolah di SMA Negeri 1 Lhokseumawe selama 3 tahun. Selama bersekolah di SMA Negeri 1 Lhokseumawe saya mengikuti organisasi siswa atau OSIS dan
+                            juga mengikuti ekstrakurikuler yaitu karate. Dan telah mengikuti dan memenangkan banyak lomba.
+                        </p>
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-primary">Juni 2017 - Mei 2020</span></div>
+                </div>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">Universitas</h3>
+                        <p>Saya mahasiswa jurusan informatika di Universitas Syiah Kuala. Saya mulai berkuliah dari bulan agustus tahun 2020 hingga sekarang. Selama berkuliah di Universitas Syiah Kuala saya mengikuti kegiatan organisasi kemahasiswaan dan
+                            beberapa kegiatan diluar akademik untuk melatih soft skill saya.
+                        </p>
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-primary">Agustus 2020 - Mei 2024</span></div>
+                </div>
+            </div>
+        </section>
+        <hr class="m-0" />
+        <!-- Education-->
+        <section class="resume-section" id="education">
+            <div class="resume-section-content">
+                <h2 class="mb-5">Education</h2>
+                <div class="d-flex flex-column flex-md-row justify-content-between">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">SMA Negeri 1 Lhokseumawe</h3>
+                        <div class="subheading mb-3">Ilmu Pengetahuan Alam</div>
+                        <p></p>
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-primary">Juni 2017 - Mei 2020</span></div>
+                </div>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">Universitas Syiah Kuala</h3>
+                        <div class="subheading mb-3">Sarjana Sains</div>
+                        <div>Computer Science - Frontend Developer</div>
+                        <p></p>
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-primary">Agustus 2020 - Mei 2024</span></div>
+                </div>
+            </div>
+        </section>
+        <hr class="m-0" />
+        <!-- Skills-->
+        <section class="resume-section" id="skills">
+            <div class="resume-section-content">
+                <h2 class="mb-5">Skills</h2>
+                <div class="subheading mb-3">Programming Languages & Tools</div>
+                <ul class="list-inline dev-icons">
+                    <li class="list-inline-item"><i class="fab fa-html5"></i></li>
+                    <li class="list-inline-item"><i class="fab fa-css3-alt"></i></li>
+                    <li class="list-inline-item"><i class="fab fa-js-square"></i></li>
+                    <li class="list-inline-item"><i class="fab fa-python"></i></li>
+                </ul>
+        </section>
+        <hr class="m-0" />
+        <!-- Interests-->
+        <section class="resume-section" id="interests">
+            <div class="resume-section-content">
+                <h2 class="mb-5">Interests</h2>
+                <p>Selain sebagai mahasiswa yang berkuliah di jurusan Informatika. Saya menikmati sebagian waktu saya untuk membaca buku dan mengembangkan skill di bidang pengembangan web.</p>
+                <p class="mb-0">Ketika libur, saya menikmati sebagian besar waktu saya untuk menonton film, membaca buku, dan juga membaca hal-hal terkait perkembangan teknologi </p>
+            </div>
+        </section>
+        <hr class="m-0" />
+        <!-- Awards-->
+        <section class="resume-section" id="awards">
+            <div class="resume-section-content">
+                <h2 class="mb-5">Awards & Certifications</h2>
+                <ul class="fa-ul mb-0">
+                    <li>
+                        <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span> Peserta Pekan Olahraga Pemuda Daerah 2016
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span> Peserta Pekan Olahraga Pemuda Daerah 2018
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span> Juara 1 - Olimpiade Olahraga Siswa Nasional Tingkat Kota Lhokseumawe 2016
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span> Juara 1 - Olimpiade Olahraga Siswa Nasional Tingkat Kota Lhokseumawe 2018
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span> Juara 1 - Olimpiade Olahraga Siswa Nasional Tingkat Kota Lhokseumawe 2016
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span> Juara 1 - Festival Indonesian Youth and Sport (IYoS) 2016
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span> Juara 3 - Lomba Cerdas Cermat (LCC) Empat Pilar MPR RI 2019
+                    </li>
+                </ul>
+            </div>
+        </section>
+        </div>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+</body>
+
+</html>
